f0d832632285873e330f002a6d3475fcb0b05912
Chris McKenzie <christophermckenzie.84@gmail.com>
2023-11-25 18:56:05 -0500

    Revert "starting point for tutorial"
    This reverts commit 304bd92f295eece79954c80402fbd8007ca36061.

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,1 +1,24 @@
-export async function GET(req) {}
+import { ChromaClient, OpenAIEmbeddingFunction } from "chromadb";
+
+const client = new ChromaClient();
+
+const embedder = new OpenAIEmbeddingFunction({
+  openai_api_key: process.env.OPENAI_API_KEY,
+});
+
+export async function GET(req) {
+  const url = new URL(req.url);
+  const query = url.searchParams.get("query");
+
+  const collection = await client.getOrCreateCollection({
+    name: "youTubeVideos",
+    embeddingFunction: embedder,
+  });
+
+  const data = await collection.query({
+    nResults: 5,
+    queryTexts: query,
+  });
+
+  return Response.json({ data });
+}
